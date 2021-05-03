e2b659c4f956a36c675ae9b2126c5ab8a1ee26cb
Kiruisang <brian.kirui@student.moringaschool.com>
2021-05-03 19:34:05 +0300

    Added daily production details

---
+++ dairy.js
@@ -0,0 +1,20 @@
+const cows = {
+    shedA : (500),
+    shedB : (300),
+    shedC : (490),
+    shedD : (570),
+    shedE : (1900)
+}
+let shedA = 500
+let shedB = 300
+let shedC = 490
+let shedD = 570
+let shedE = 1900
+
+// let totalProduction = ( "shedA + shedB + shedC + shedD + shedE")
+
+console.log("Your production in  shed A " + shedA + " litres per day")
+console.log("Your production in  shed B " + shedB + " litres per day")
+console.log("Your production in  shed C " + shedC + " litres per day")
+console.log("Your production in  shed D " + shedD + " litres per day")
+console.log("Your production in  shed E " + shedE + " litres per day")
